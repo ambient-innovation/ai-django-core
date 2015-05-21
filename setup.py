20e850821c10cae0f3a0df2add98564f1eab0efb
Dennis Mosemann <dennis.mosemann@ambient-innovation.com>
2015-05-21 17:58:27 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from distutils.core import setup
-from setuptools import find_packages
+from setuptools import setup, find_packages
 
 setup(
     name='ai-django-core',
@@ -8,9 +8,11 @@ setup(
     author=u'Ambient Innovation GmbH',
     author_email='info@ambient-innovation.com',
     packages=find_packages(),
+    include_package_data=True,
     url='ssh://git@gitlab.ambient-innovation.com:20141/ai/ai-django-core',
     license='BSD License',
     description='Lots of helper functions and useful widgets.',
     long_description=open('README.rst').read(),
     zip_safe=False,
+    install_requires=[]
 )
