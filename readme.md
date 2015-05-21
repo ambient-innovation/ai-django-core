c1c39fcc64b81c8f354257c1cff1d6d09b5a914b
Dennis Mosemann <dennis.mosemann@ambient-innovation.com>
2015-05-21 17:32:03 +0200

    init

---
+++ readme.md
@@ -0,0 +1,4 @@
+Pip constructed with the help of: http://bruno.im/2010/may/05/packaging-django-reusable-app/
+
+
+
---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='ai-django-core',
+    version='1.0',
+    author=u'Ambient Innovation GmbH',
+    author_email='info@ambient-innovation.com',
+    packages=find_packages(),
+    url='ssh://git@gitlab.ambient-innovation.com:20141/ai/ai-django-core',
+    license='',
+    description='Lots of helper functions and useful widgets.',
+    long_description=open('readme.md').read(),
+    zip_safe=False,
+)
