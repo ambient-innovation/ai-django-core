277a7feb849b2ef11563d8abc3a2ffd5778bc922
Marius Burfey <marius.burfey@ambient-innovation.com>
2016-05-18 14:42:37 +0200

    removed debugging-stuff

--- ai/mail/backends/whitelist_smtp.py
+++ ai/mail/backends/whitelist_smtp.py
@@ -3,29 +3,22 @@ import re
 
 from django.conf import settings
 from django.core.mail.backends.smtp import EmailBackend as SMTPEmailBackend
-from django.core.mail.backends.console import EmailBackend as ConsoleEmailBackend
 
 EMAIL_BACKEND_DOMAIN_WHITELIST = getattr(settings, 'EMAIL_BACKEND_DOMAIN_WHITELIST', [])
 REGEX = r'^[\w\-\.]+@(%s)$' % '|'.join(x for x in EMAIL_BACKEND_DOMAIN_WHITELIST).replace('.', '\.')
 
 
-class EmailBackend(SMTPEmailBackend):
+class WhitelistEmailBackend(SMTPEmailBackend):
     @staticmethod
     def whitify_mail_adresses(mail_adress_list):
         allowed_recipients = []
-        # unallowed_recipients = []
         for to in mail_adress_list:
             if re.search(REGEX, to):
                 allowed_recipients.append(to)
             else:
                 # sende nicht erlaubte Mails an den testuser.t-ped.de Catchall
                 allowed_recipients.append("%s@testuser.t-ped.de" % to.replace('@', '_'))
-                # unallowed_recipients.append(to)
         return allowed_recipients
-
-    def __init__(self, *args, **kwargs):
-        self.console_backend = ConsoleEmailBackend(*args, **kwargs)
-        super(EmailBackend, self).__init__(*args, **kwargs)
 
     def send_messages(self, email_messages):
         """
@@ -35,5 +28,4 @@ class EmailBackend(SMTPEmailBackend):
         for email in email_messages:
             allowed_recipients = self.whitify_mail_adresses(email.to)
             email.to = allowed_recipients
-        # super(EmailBackend, self).send_messages(email_messages)
-        self.console_backend.send_messages(email_messages)
+        super(WhitelistEmailBackend, self).send_messages(email_messages)
