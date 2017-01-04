5bd9ae4ae3ce512844a735cdc35c4a575237bd18
Dhia Abbassi <dhia.abbassi@ambient-innovation.com>
2017-01-04 16:49:45 +0100

    Updated package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ai-django-core',
-    version='1.0.8.0',
+    version='1.0.8.1',
     author=u'Ambient Innovation GmbH',
     author_email='info@ambient-innovation.com',
     packages=find_packages(),
