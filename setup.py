8e9c5fca09fd3a8bbf2a73e417d9adc2d92ef120
Felix Schul <felix.schul@ambient-innovation.com>
2018-01-02 14:16:12 +0100

    URL changed to public HTTPS

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     author_email='hello@ambient-innovation.com',
     packages=find_packages(),
     include_package_data=True,
-    url='ssh://git@gitlab.ambient-innovation.com:20141/ai/ai-django-core',
+    url='https://gitlab.ambient-innovation.com/ai/ai-django-core.git',
     license="The MIT License (MIT)",
     description='Lots of helper functions and useful widgets.',
     long_description=open('README.md').read(),
