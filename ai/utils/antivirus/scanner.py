c1784eeef124f0e5aab001152c3fc9982821e0ab
Timothy C Eichler <timeyyy_da_man@hotmail.com>
2018-02-26 16:50:23 +0100

    virusscanner: add option to controll scanning with single core

--- ai/utils/antivirus/scanner.py
+++ ai/utils/antivirus/scanner.py
@@ -8,13 +8,24 @@ class AVScanner(object):
     def __init__(self):
         self.multi_av = CMultiAV("%s/cmultiav.cfg" % os.path.dirname(__file__))
 
-    def has_virus(self, file):
+    def has_virus(self, file, parallel=True):
         with tempfile.NamedTemporaryFile(delete=False) as temporaryfile:
             temporaryfile.write(file.read())
             temporaryfile.close()
             permission = 0o664 # PEP 3127: octal literals
             os.chmod(temporaryfile.name, permission)  # clamav needs permission to scan
-            ret = self.multi_av.scan(temporaryfile.name, AV_SPEED_MEDIUM)
+            if parallel:
+                ret = self.multi_av.scan(temporaryfile.name, AV_SPEED_MEDIUM)
+            else:
+
+                try:
+                    ret = self.multi_av.single_scan(temporaryfile.name, AV_SPEED_MEDIUM)
+                except OSError as err:
+                    # It would seem a scanner is not installed...
+                    # We don't need to check this when in parallel
+                    # As the main process is still alive
+                    return False, None
+
             os.unlink(temporaryfile.name)
             for x in ret.values():
                 if x != {}:
--- ai/utils/antivirus/storage.py
+++ ai/utils/antivirus/storage.py
@@ -8,8 +8,12 @@ av_scanner = AVScanner()
 
 
 class AVFileSystemStorage(FileSystemStorage):
+    parallel = True
     def save(self, name, content, max_length=None):
-        av_result = av_scanner.has_virus(content)
+        av_result = av_scanner.has_virus(content, parallel=self.parallel)
         if av_result[0] is False:
             return super(AVFileSystemStorage, self).save(name, content, max_length)
         raise AVException(av_result[1])
+
+class SingleCoreAVFileSystemStorage(AVFileSystemStorage):
+    parallel = False
