899b5c949c58aefa910cfb7d93bbb3b7b317ce0c
Timothy C Eichler <timeyyy_da_man@hotmail.com>
2018-02-27 11:24:42 +0100

    virusscanner: toggle scanner with django env

--- ai/utils/antivirus/storage.py
+++ ai/utils/antivirus/storage.py
@@ -1,19 +1,24 @@
 # -*- coding: utf-8 -*-
 from django.core.files.storage import FileSystemStorage
+from django.conf import settings
 
 from ai.utils.antivirus import AVException
 from .scanner import AVScanner
 
 av_scanner = AVScanner()
 
+USE_SCANNER = settings.ANTIVIRUS_ENABLED if hasattr(settings, 'ANTIVIRUS_ENABLED') else True
 
 class AVFileSystemStorage(FileSystemStorage):
     parallel = True
     def save(self, name, content, max_length=None):
-        av_result = av_scanner.has_virus(content, parallel=self.parallel)
-        if av_result[0] is False:
+        if USE_SCANNER:
+            av_result = av_scanner.has_virus(content, parallel=self.parallel)
+            if av_result[0] is False:
+                return super(AVFileSystemStorage, self).save(name, content, max_length)
+            raise AVException(av_result[1])
+        else:
             return super(AVFileSystemStorage, self).save(name, content, max_length)
-        raise AVException(av_result[1])
 
 class SingleCoreAVFileSystemStorage(AVFileSystemStorage):
     """
