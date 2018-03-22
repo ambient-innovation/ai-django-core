18ff6dbadf968aa84ca9a3e0e60018724da7a828
Marius Burfey <marius.burfey@ambient-innovation.com>
2018-03-22 16:41:25 +0100

    Bugfix: AV-Scanner komplett deaktivierbar

--- ai/utils/antivirus/field.py
+++ ai/utils/antivirus/field.py
@@ -5,16 +5,18 @@ from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from .storage import USE_SCANNER
 from .scanner import AVScanner
 
 scanner = AVScanner()
 
 
 def file_validator(f):
-    has_virus, name = scanner.has_virus(f.file)
-    if has_virus:
-        raise ValidationError(_('In dieser Datei wurde ein Virus erkannt.'))
-        # raise ValidationError(_('Virus "{}" wurde erkannt.').format(name))
+    if USE_SCANNER:
+        has_virus, name = scanner.has_virus(f.file)
+        if has_virus:
+            raise ValidationError(_('In dieser Datei wurde ein Virus erkannt.'))
+            # raise ValidationError(_('Virus "{}" wurde erkannt.').format(name))
 
 
 class AVProtectedFileField(models.FileField):
