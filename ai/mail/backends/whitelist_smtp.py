286f25ddb634c74185eb4d76d1b3095252ac1823
Marius Burfey <marius.burfey@ambient-innovation.com>
2018-04-27 08:19:17 +0200

    Whitelist-Mail-Backend konfigurierbar

--- ai/mail/backends/whitelist_smtp.py
+++ ai/mail/backends/whitelist_smtp.py
@@ -5,19 +5,32 @@ from django.conf import settings
 from django.core.mail.backends.smtp import EmailBackend as SMTPEmailBackend
 
 EMAIL_BACKEND_DOMAIN_WHITELIST = getattr(settings, 'EMAIL_BACKEND_DOMAIN_WHITELIST', [])
+EMAIL_BACKEND_REDIRECT_ADDRESS = getattr(settings, 'EMAIL_BACKEND_REDIRECT_ADDRESS')
 REGEX = r'^[\w\-\.]+@(%s)$' % '|'.join(x for x in EMAIL_BACKEND_DOMAIN_WHITELIST).replace('.', '\.')
 
 
 class WhitelistEmailBackend(SMTPEmailBackend):
+    """
+    Man kann über die folgenden Settings konfigurieren, ob Mails an _alle_ Adressen verschickt werden.
+    Wenn nicht, dann kann eine Umleitung an ein Postfach mit CATCHALL konfiguriert werden.
+
+    EMAIL_BACKEND = 'ai.mail.backends.whitelist_smtp.WhitelistEmailBackend'
+    EMAIL_BACKEND_DOMAIN_WHITELIST = ['ambient-innovation.com']
+    EMAIL_BACKEND_REDIRECT_ADDRESS = '%s@testuser.ambient-innovation.com'
+
+    Wenn EMAIL_BACKEND_REDIRECT_ADDRESS gesetzt ist, wird eine Mail an xy.z@example.com umgeleitet an:
+    xy.z_example.com@testuser.ambient-innovation.com
+    """
+
     @staticmethod
     def whitify_mail_adresses(mail_adress_list):
         allowed_recipients = []
         for to in mail_adress_list:
             if re.search(REGEX, to):
                 allowed_recipients.append(to)
-            else:
-                # sende nicht erlaubte Mails an den testuser.t-ped.de Catchall
-                allowed_recipients.append("%s@testuser.t-ped.de" % to.replace('@', '_'))
+            elif EMAIL_BACKEND_REDIRECT_ADDRESS:
+                # sende nicht erlaubte Mails an die angegebene Redirect-Adresse (mit CATCHALL)
+                allowed_recipients.append(EMAIL_BACKEND_REDIRECT_ADDRESS % to.replace('@', '_'))
         return allowed_recipients
 
     def send_messages(self, email_messages):
