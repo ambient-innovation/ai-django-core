84e7d28b0ea6384fa419b39c5f806f1e8893e8fb
Ronny Vedrilla <ronny.vedrilla@ambient-innovation.com>
2018-08-24 14:01:11 +0200

    Versionstag angepasst

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = [
 
 setup(
     name='ai-django-core',
-    version='1.1.7',
+    version='1.1.8',
     author=u'Ambient Innovation GmbH',
     author_email='hello@ambient-innovation.com',
     packages=find_packages(),
